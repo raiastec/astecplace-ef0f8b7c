7918501e09bbac97731dc125478aac6db03cce2d
Astec assessoria <raiani.diniz.astec@gmail.com>
2025-10-24 20:58:14 +0000

    alteração

--- src/components/sections/BanksCarousel.tsx
+++ src/components/sections/BanksCarousel.tsx
@@ -3,24 +3,24 @@ import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
 import bancoDoBrasil from "@/assets/banks/banco_brasil_rural.png";
 import caixa from "@/assets/banks/caixa_rural.png";
-// import santander from "@/assets/banks/santander.png";
-// import bradesco from "@/assets/banks/bradesco.png";
-// import itau from "@/assets/banks/itau.png";
+import santander from "@/assets/banks/santander.png";
+import bradesco from "@/assets/banks/bradesco.jpg";
+import itau from "@/assets/banks/itau.png";
 import sicoob from "@/assets/banks/sicoob_rural.png";
 import sicredi from "@/assets/banks/credisis_rural.png";
-// import bv from "@/assets/banks/bv.png";
+import bv from "@/assets/banks/bv.jpg";
 import cresol from "@/assets/banks/cresol.png";
-import bancoAmazonia from "@/assets/banks/Banco_da_Amazônia.png";
+import bancoAmazonia from "@/assets/banks/Banco_da_Amazônia.jpg";
 
 const banks = [
   { name: "Banco do Brasil", logo: bancoDoBrasil },
   { name: "Caixa Econômica", logo: caixa },
- // { name: "Santander", logo: santander },
- // { name: "Bradesco", logo: bradesco },
- // { name: "Itaú", logo: itau },
+  { name: "Santander", logo: santander },
+  { name: "Bradesco", logo: bradesco },
+  { name: "Itaú", logo: itau },
   { name: "Sicoob", logo: sicoob },
   { name: "Sicredi", logo: sicredi },
-  // { name: "BV Financeira", logo: bv },
+  { name: "BV Financeira", logo: bv },
   { name: "Cresol", logo: cresol },
   { name: "Banco da Amazônia", logo: bancoAmazonia },
 ];
